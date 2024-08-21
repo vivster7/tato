7645d1c391a7375ec601a6761022e34864f47f0f
Vivek Dasari <vivster7@gmail.com>
2024-08-21 06:22:14 +0000

    only count external references

--- src/tato/index/__tests__/test_index.py
+++ src/tato/index/__tests__/test_index.py
@@ -5,20 +5,20 @@ from tato.index.index import Index
 PARENT = Path(__file__).parent
 
 
-def test_indexer():
+def test_index():
     package = PARENT.joinpath("data/index/test1")
     dbpath = package.joinpath("tato-index.sqlite3")
     if dbpath.exists():
         dbpath.unlink()
 
-    index = Index(package)
+    index = Index(dbpath)
 
     assert index.count_references("does.not.exist") == 0
 
-    index.create(package)
+    index.create()
 
     assert index.count_references("does.not.exist") == 0
-    assert index.count_references("test1.a.one") == 3
+    assert index.count_references("test1.a.one") == 2
     assert index.count_references("test1.b.one") == 1
-    assert index.count_references("test1.b.two") == 2
-    assert index.count_references("test1.c.three") == 1
+    assert index.count_references("test1.b.two") == 1
+    assert index.count_references("test1.c.three") == 0
--- src/tato/index/index.py
+++ src/tato/index/index.py
@@ -13,34 +13,35 @@ class Index:
         self._has_index = self.db.path.exists() and self.db.path.stat().st_size > 0
 
     def count_references(self, fully_qualified_name: str) -> int:
-        """Counts all references for a fully_qualified_name.
+        """Counts all external references for a fully_qualified_name.
 
-        What counts as a reference?
-          Original definitions count as a reference.
-          Usages count as references.
-          Aliased usages count as references.
-          Imports do NOT count as references.
+        What counts as an external reference?
+            Usages in a different file from the original definition count as external references.
+            Aliased usages in a different file count as external references.
+            Imports do NOT count as references.
+            References in the same file as the original definition do NOT count as external references.
         """
         if not self._has_index:
             return 0
         res = self.db.cursor.execute(
             """
-            WITH RECURSIVE all_definitions(id) AS (
+            WITH RECURSIVE all_definitions(id, original_file_id) AS (
                 -- Start with the original definition
-                SELECT id FROM Definition WHERE Definition.fully_qualified_name = ?
+                SELECT id, file_id FROM Definition WHERE Definition.fully_qualified_name = ?
                 
                 UNION ALL
                 
                 -- Recursively add all definitions that import this definition
-                SELECT dd.to_definition_id
+                SELECT dd.to_definition_id, ad.original_file_id
                 FROM DefDef dd
                 JOIN all_definitions ad ON dd.from_definition_id = ad.id
             )
             SELECT COUNT(DISTINCT r.id) as reference_count
             FROM all_definitions ad
             JOIN DefRef dr ON dr.definition_id = ad.id
-            JOIN Reference r ON r.id = dr.reference_id;        
-        """,
+            JOIN Reference r ON r.id = dr.reference_id
+            WHERE r.file_id != ad.original_file_id;
+            """,
             (fully_qualified_name,),
         )
         return res.fetchone()[0]
